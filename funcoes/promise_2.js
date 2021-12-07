fbdc782f06195105b0bc2eaf2cd15240210e260d
William Viana <willviana.batera@gmail.com>
2021-12-07 16:55:30 -0300

    undestanding promise-2

---
+++ funcoes/promise_2.js
@@ -0,0 +1,27 @@
+// Callback Hell aninhamento infinito 
+// setTimeout(function () {
+//     console.log('Executando callback...')
+
+//     setTimeout(function () {
+//         console.log('Executando callback 2...')
+
+//         setTimeout(function () {
+//             console.log('Executando callback 3...')
+
+//         }, 2000)
+//     }, 2000)
+// }, 2000)
+
+function esperarPor(tempo = 2000) {
+    return new Promise(function (resolve) {
+        setTimeout(function () {
+            console.log('Executando Promise...')
+            resolve()
+        }, tempo)
+    })
+}
+
+esperarPor()
+    .then(() => esperarPor(3000))
+    .then(() => esperarPor(5000))
+    .then(esperarPor)
