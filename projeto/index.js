c91bfd063513dbf7ca6cb7c1d99bb232042cc744
William Viana <willviana.batera@gmail.com>
2021-12-10 12:32:37 -0300

    create index for develop

---
+++ projeto/index.js
@@ -0,0 +1,10 @@
+const path = require('path')
+const fn = require('./funcoes')
+const fs = require('fs')
+
+const caminho = path.join(__dirname, '..', 'dados', 'legendas')
+
+fn.lerDiretorio(caminho)
+  .then(arquivos => fn.elementosTerminadosCom(arquivos, '.srt'))
+  .then(arquivosSRT => fn.lerArquivos(arquivosSRT))
+  .then(console.log)
