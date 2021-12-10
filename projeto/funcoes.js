eb00ebbcfbdb6a43b2b13f41973f0ed42deeea6b
William Viana <willviana.batera@gmail.com>
2021-12-10 12:32:16 -0300

    create file with functions for index

---
+++ projeto/funcoes.js
@@ -0,0 +1,54 @@
+const fs = require('fs')
+const path = require('path')
+
+function lerDiretorio(caminho) {
+  return new Promise((resolve, reject) => {
+    try {
+      let arquivos = fs.readdirSync(caminho) // retorna um array
+      arquivos = arquivos.map(arquivo => path.join(caminho, arquivo))
+      resolve(arquivos)
+    } catch (e) {
+      reject(e)
+    }
+  })
+}
+
+function elementosTerminadosCom(array, padrao) {
+  return array.filter(el => el.endsWith(padrao))
+}
+
+// function lerConteudosDosArquivos(conteudo) {
+//   return new Promise((resolve, reject) => {
+//     try {
+//       let conteudos = fs.readFile(conteudo, (err, conteudo) => {
+//         conteudo.toString()
+//       },
+//         resolve(conteudos))
+//     } catch (e) {
+//       reject(e)
+//     }
+//   })
+// }
+
+function lerArquivo(caminho) {
+  return new Promise((resolve, reject) => {
+    try {
+      const conteudo = fs.readFileSync(caminho, { encoding: 'utf-8' })
+      resolve(conteudo.toString())
+    } catch (e) {
+      reject(e)
+    }
+
+  })
+}
+
+function lerArquivos(caminhos) {
+  return Promise.all(caminhos.map(caminho => lerArquivo(caminho)))
+}
+
+module.exports = {
+  lerDiretorio,
+  lerArquivo,
+  lerArquivos,
+  elementosTerminadosCom,
+}
