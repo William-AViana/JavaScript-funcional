987178db068ef3cd6b670ab375cc786dc1c899f8
William Viana <willviana.batera@gmail.com>
2021-12-14 20:39:38 -0300

    add const and call functions

--- projeto/index.js
+++ projeto/index.js
@@ -4,7 +4,26 @@ const fs = require('fs')
 
 const caminho = path.join(__dirname, '..', 'dados', 'legendas')
 
+const simbolos = [
+  '.', '?', '-', ',', '"', '♪',
+  '_', '<i>', '</i>', '\r', '[', ']',
+  '(', ')'
+]
+
 fn.lerDiretorio(caminho)
-  .then(arquivos => fn.elementosTerminadosCom(arquivos, '.srt'))
+  .then(fn.elementosTerminadosCom('.srt')) // exemplo de refatoração
   .then(arquivosSRT => fn.lerArquivos(arquivosSRT))
+  .then(fn.mesclarElementos)
+  .then(fn.separarTextoPor('\n'))
+  .then(linhas => fn.removerElementosSeVazio(linhas))
+  .then(fn.removerElementosSeIncluir('-->'))
+  .then(fn.removerElementosSeApenasNumeros)
+  .then(fn.removerSimbolos(simbolos))
+  .then(fn.mesclarElementos)
+  .then(fn.separarTextoPor(' '))
+  .then(fn.removerElementosSeVazio) // reuso de uma função
+  .then(fn.removerElementosSeApenasNumeros)
+  .then(fn.agruparElementos)
+  .then(fn.ordenarPorAtributoNumerico('qtde', 'desc'))
+  // desafio tranformar em um arquivo
   .then(console.log)
