062c11f458452d9b7088445d51e984f8496290ab
William Viana <willdev.viana@gmail.com>
2022-04-22 21:40:35 -0300

    simple stream of data v2

--- rxjs/stream_dados1.js
+++ rxjs/stream_dados1.js
@@ -1,14 +1,30 @@
-function gerarNumeros(fn) {
-  let num = 0
-  setInterval(() => {
-    fn(num++)
-  }, 1000);
+function gerarNumeros() {
+  return {
+    init(fn, interval = 1000) {
+      let num = 0
+      const timer = setInterval(() => {
+        fn(num++)
+      }, interval);
+      return {
+        stop() {
+          clearInterval(timer)
+        }
+      }
+    }
+  }
 };
 
-gerarNumeros(numero => {
-  console.log(`#1: ${numero * 2}`);
+const temp1 = gerarNumeros();
+const exec1 = temp1.init(num => {
+  console.log(`#1: ${num * 2}`);
 });
 
-gerarNumeros(n => {
-  console.log(`#2: ${n + 10}`);
-});
+const temp2 = gerarNumeros();
+const exec2 = temp2.init(num => {
+  console.log(`#2: ${num + 10}`);
+}, 2000);
+
+setTimeout(() => {
+  exec1.stop();
+  exec2.stop();
+}, 10000)
